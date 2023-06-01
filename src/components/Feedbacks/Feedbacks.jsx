5adbcc5f406980c01a32bc76d351827c8157e3e5
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-06-01 13:04:22 +0300

    Update Feedbacks.jsx

--- src/components/Feedbacks/Feedbacks.jsx
+++ src/components/Feedbacks/Feedbacks.jsx
@@ -1,79 +1,76 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
 import css from './Feedbacks.module.css';
 import FeedbackOptions from './FeedbackOptions';
 
 class Feedbacks extends Component {
-    static defaultProps = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-    }
+  static defaultProps = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
 
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-        total: 0
-    };
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
 
-    onLeaveFeedback = (type) => {
-        this.setState((previousValue) => {
-            return {
-                [type]: previousValue[type] + 1,
-            }
-        }, this.countTotalFeedback);
-    }
+  onLeaveFeedback = (type) => {
+    this.setState((previousValue) => {
+      return {
+        [type]: previousValue[type] + 1
+      };
+    });
+  };
 
-    countTotalFeedback = () => {
-        const { good, neutral, bad } = this.state;
-        const feedbackTotal = good + neutral + bad;
-        this.setState({
-            total: feedbackTotal
-        });
-    }
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const feedbackTotal = good + neutral + bad;
+    return feedbackTotal;
+  };
 
-    countPositiveFeedbackPercentage() {
-        const { good, total } = this.state;
-        const positiveFeedbackPercentage = total === 0 ? 0 : ((good / total) * 100).toFixed(0);
-        return positiveFeedbackPercentage;
-    }
+  countPositiveFeedbackPercentage() {
+    const { good } = this.state;
+    const feedbackTotal = this.countTotalFeedback();
+    const positiveFeedbackPercentage =
+      feedbackTotal === 0 ? 0 : ((good / feedbackTotal) * 100).toFixed(0);
+    return positiveFeedbackPercentage;
+  }
 
-    render() {
-        const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
+  render() {
+    const feedbackTotal = this.countTotalFeedback();
+    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
 
-        return (
-            <>
-                <FeedbackOptions
-                    options={['good', 'neutral', 'bad']}
-                    onLeaveFeedback={this.onLeaveFeedback}
-                />
+    return (
+      <>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
 
-                <p className={css.description}>Statistics</p>
-                <ul className={css.feedbackValues}>
-                    <li>
-                        <span className={css.good}>Good: {this.state.good}</span>
-                    </li>
-                    <li>
-                        <span className={css.neutral}>Neutral: {this.state.neutral}</span>
-                    </li>
-                    <li>
-                        <span className={css.bad}>Bad: {this.state.bad}</span>
-                    </li>
-                    <li>
-                        <span className={css.feedbackTotal}>Total: {this.state.total}</span>
-                    </li>
-                    <li>
-                        {this.state.total === 0 ? (
-                            <span className={css.positiveFeedbacks}>Positive feedback: 0%</span>)
-                            :
-                            (<span className={css.positiveFeedbacks}>Positive feedback: {positiveFeedbackPercentage}%</span>
-                        )}
-                    </li>
-                </ul>
-            </>
-        )
-    }
+        <p className={css.description}>Statistics</p>
+        <ul className={css.feedbackValues}>
+          <li>
+            <span className={css.good}>Good: {this.state.good}</span>
+          </li>
+          <li>
+            <span className={css.neutral}>Neutral: {this.state.neutral}</span>
+          </li>
+          <li>
+            <span className={css.bad}>Bad: {this.state.bad}</span>
+          </li>
+          <li>
+            <span className={css.feedbackTotal}>Total: {feedbackTotal}</span>
+          </li>
+          <li>
+            <span className={css.positiveFeedbacks}>
+              Positive feedback: {positiveFeedbackPercentage}%
+            </span>
+          </li>
+        </ul>
+      </>
+    );
+  }
 }
 
 export default Feedbacks;
